26bd66ca5fbcad221edb1ee42ab78f155d567fc1
albert <albert>
2004-04-23 13:27:44 +0000

    All relative possibilities are tested. Non-labels are indented.

--- test.asm
+++ test.asm
@@ -1,16 +1,25 @@
 
 
-1278 ORG
 ASSEMBLER
-CLD,
-MOV, X| T| DI'| MEM| XXX X,
+1278 ORG
+    CLD,
+    MOV, X| T| DI'| MEM| XXX X,
 :QQQ
-POP|ES,
-ADD, B| F| AL'| D0| [SI]
-MOV, X| T| DI'| MEM| XXX X,
+    POP|ES,
+    ADD, B| F| AL'| D0| [SI]
+    MOV, X| T| DI'| MEM| XXX X,
 :XXX
-MOV, X| T| DI'| MEM| QQQ X,
-JMP, XXX _AP_ 4 + - (RX,)
-JMP, XXX RX,
-JMP, XXX 1- RX,
+    MOV, X| T| DI'| MEM| QQQ X,
+    JMP, XXX RX,
+    JMP, XXX _AP_ 4 + - (RX,)
+    JMP, XXX 1- RX,
+    JMPS, XXX RB,
+    JMPS, XXX 1- RB,
+    JMP, YYY RX,
+    JMP, YYY 1- RX,
+    JMPS, YYY RB,
+    JMPS, YYY 1- RB,
+    LEA, AX'| DB| [AX +4* AX] 0 B,
+:YYY
+
 PREVIOUS
